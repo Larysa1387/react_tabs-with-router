f561eb8495345f542a5bb4f67a46e8b5a5dc7687
larysa1387 <kaliuzhna.off@gmail.com>
2025-12-01 22:39:37 +0200

    Refactor Tabs implementation and update routing in TabsPage component

--- src/Root.tsx
+++ src/Root.tsx
@@ -8,11 +8,10 @@ export const Root = () => (
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<HomePage />} />
-        <Route path="/home" element={<Navigate to={'/'} />} />
-        {/* <Route path="/tabs" element={<TabsPage />} /> */}
+        <Route path="home" element={<Navigate to={'/'} />} />
         <Route path="tabs">
           <Route index element={<TabsPage />} />
-          <Route path=":tabId?" element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
         </Route>
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
       </Route>
--- src/components/TabsPage.tsx
+++ src/components/TabsPage.tsx
@@ -1,41 +1,8 @@
-import { Link, Outlet, useParams } from 'react-router-dom';
-import { Tab } from '../types/Tab';
+import { Tabs } from './Tabs';
 
-const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
-
-export const TabsPage: React.FC = () => {
-  const { tabId } = useParams();
-
-  const tab = tabId || '';
-  const selectedTab = tabs.find(t => t.id === tab);
-
-  return (
-    <>
-      <h1 className="title">Tabs page</h1>
-      <div>
-        <div className="tabs is-boxed">
-          <ul>
-            {tabs.map(t => (
-              <li
-                data-cy="Tab"
-                key={t.id}
-                className={t.id === selectedTab?.id ? 'is-active' : ''}
-              >
-                <Link to={`/tabs/${t.id}`}>{t.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="block" data-cy="TabContent">
-          {selectedTab ? selectedTab.content : 'Please select a tab'}
-        </div>
-
-        <Outlet />
-      </div>
-    </>
-  );
-};
+export const TabsPage: React.FC = () => (
+  <>
+    <h1 className="title">Tabs page</h1>
+    <Tabs />
+  </>
+);
